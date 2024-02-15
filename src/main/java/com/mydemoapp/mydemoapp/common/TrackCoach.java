94c37dab01284eb2ab64f0ede1399e9742a66418
Omar Sayed <orhman@hotmail.com>
2024-02-15 15:34:44 +0200

    using @Primary for default injection

--- src/main/java/com/mydemoapp/mydemoapp/common/TrackCoach.java
+++ src/main/java/com/mydemoapp/mydemoapp/common/TrackCoach.java
@@ -1,8 +1,13 @@
 package com.mydemoapp.mydemoapp.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+// if multiple implementation of Coach are present, this on will be picked
+// multiple primaries will fail
+// if @Qualifier is used it will have higher precedence
+@Primary
 public class TrackCoach implements Coach{
     @Override
     public String getDailyWorkout() {
--- src/main/java/com/mydemoapp/mydemoapp/rest/DemoController.java
+++ src/main/java/com/mydemoapp/mydemoapp/rest/DemoController.java
@@ -2,13 +2,12 @@ package com.mydemoapp.mydemoapp.rest;
 
 import com.mydemoapp.mydemoapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    private Coach myCoach;
+    private final Coach myCoach;
 
     //    autowire is optional when 1 constructor is defined
     // currently only 1 coach is defined so autowire imports the CricketCoach
@@ -18,10 +17,10 @@ public class DemoController {
     // DemoController demoController = new DemoController(myCoach)
 //    -----------------------------------------
 // constructor injection (recommended)
-//    @Autowired
-//    public DemoController(Coach theCoach) {
-//        myCoach = theCoach;
-//    }
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
 //    -----------------------------------------
 
 //    -----------------------------------------
@@ -30,10 +29,10 @@ public class DemoController {
     // (as long as it's annotated with autowired it can be named to anything)
 // when we have multiple beans that implements Coach spring will fail to know which to inject
 // we have to use the qualifier to determine which, notice it starts with lower case
-    @Autowired
-    public void setCoach(@Qualifier("trackCoach") Coach theCoach){
-        myCoach = theCoach;
-    }
+//    @Autowired
+//    public void setCoach(@Qualifier("trackCoach") Coach theCoach){
+//        myCoach = theCoach;
+//    }
 //    -----------------------------------------
 
 //    -----------------------------------------
